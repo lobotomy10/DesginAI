7d1ad73856d73a4941c7b8903aa6fcfefef6ffc4
lobotomy10 <takumori0120@icloud.com>
2025-05-30 00:19:42 +0900

    copy some files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'bounce-slow': 'bounce 2s infinite',
+      },
+      fontFamily: {
+        'mono': ['Monaco', 'Menlo', 'Ubuntu Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
